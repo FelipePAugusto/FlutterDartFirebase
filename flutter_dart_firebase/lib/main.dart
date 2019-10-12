a9ecd70f3c107a671b9abab66d6b2422c6d2de92
Felipe <felipe.augusto.ext@sascar.com.br>
2019-10-12 13:49:11 -0300

    Bibliotecas do Google Sign in
    Foi incluido as bibliotecas do google sign in no pubspec.yaml

--- flutter_dart_firebase/lib/main.dart
+++ flutter_dart_firebase/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main(){
-  Firestore.instance.collection("teste").document("teste").setData({"teste" : "teste"});
+  Firestore.instance.collection("banco_central").document("usuarios").setData({"nome" : "Felipe"});
   
   runApp(MyApp()); 
 }
